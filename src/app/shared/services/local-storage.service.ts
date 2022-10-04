b4a9f4229177b0195fe4de7975d6608bf6fb2dac
Demyan <dyo171717@gmail.com>
2022-10-04 22:53:55 +0300

    feat(shared): created Local Storage Service

---
+++ src/app/shared/services/local-storage.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class LocalStorageService {
+
+	public getItem(key: string): unknown {
+		return JSON.parse(localStorage.getItem(key) || '');
+	}
+
+	public setItem(key: string, data: unknown): void {
+		localStorage.setItem(key, JSON.stringify(data));
+	}
+}
