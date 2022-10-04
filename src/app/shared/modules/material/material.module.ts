a379cccbfd7b08349ce0218d3f995c4990de60d8
Demyan <dyo171717@gmail.com>
2022-10-04 19:06:24 +0300

    feat(app): created Maredial and UI modules

--- src/app/shared/modules/material/material.module.ts
+++ src/app/shared/modules/material/material.module.ts
@@ -1,12 +1,34 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
 
 
+const MaterialComponents = [
+	MatIconModule,
+	MatButtonModule,
+	MatFormFieldModule,
+	MatInputModule,
+	MatCheckboxModule,
+	MatDialogModule,
+	MatTableModule,
+	MatSortModule,
+	MatDatepickerModule,
+	MatNativeDateModule,
+	MatSelectModule
+];
 
 @NgModule({
-	declarations: [],
-	imports: [
-		CommonModule
-	]
+	imports: [MaterialComponents],
+	exports: [MaterialComponents]
 })
 export class MaterialModule { }
---
+++ src/app/shared/shared/shared.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MaterialModule } from '../modules/material/material.module';
+import { UIModule } from '../modules/UI/ui.module';
+
+
+
+@NgModule({
+	declarations: [],
+	imports: [
+		CommonModule,
+		MaterialModule,
+		UIModule
+	],
+	exports: [
+		MaterialModule,
+		UIModule
+	]
+})
+export class SharedModule { }
