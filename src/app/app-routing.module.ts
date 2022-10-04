016182b147823791df3e28d4be04a39dc94f029c
Demyan <dyo171717@gmail.com>
2022-10-04 18:47:15 +0300

    feat(app): created base modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: '',
+		loadChildren: () => import('./modules/task/task.module').then(module => module.TaskModule)
+	},
+	{
+		path: 'auth',
+		loadChildren: () => import('./modules/auth/auth.module').then(module => module.AuthModule)
+	},
+	{
+		path: 'categories',
+		loadChildren: () => import('./modules/category/category.module').then(module => module.CategoryModule)
+	},
+	{
+		path: '**',
+		redirectTo: '/'
+	}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
