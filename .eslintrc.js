3c370291fc51a8c1ae748eef15cd492e40062e08
Demyan <dyo171717@gmail.com>
2022-11-19 18:02:15 +0300

    update (eslint): added new rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,15 +7,11 @@ module.exports = {
 	'overrides': [{
 		'files': [
 			'*.ts'
-		],
+		]
 	}],
 	'extends': [
 		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:lit/recommended',
-		// 'airbnb-typescript/base',
-		// 'prettier/@typescript-eslint',
-		// 'plugin:prettier/recommended'
+		'plugin:@typescript-eslint/recommended'
 	],
 	'parser': '@typescript-eslint/parser',
 	'parserOptions': {
@@ -26,27 +22,25 @@ module.exports = {
 		'@typescript-eslint'
 	],
 	'rules': {
-		'indent': [
-			'error',
-			'tab'
-		],
-		'linebreak-style': [
-			'error',
-			'unix'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'always'
-		],
-		'comma-dangle': ['error', {
-			'arrays': 'never',
-			'objects': 'never'
-		}],
+		'@typescript-eslint/explicit-function-return-type': 'error',
+		'@typescript-eslint/no-unused-vars': 'error',
 		'max-lines': 2,
-		'max-lines-per-function': 2
+		'max-lines-per-function': 2,
+		'indent': ['error', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		'quotes': ['error', 'single'],
+		'semi': ['error', 'always'],
+		'comma-dangle': ['error', {'arrays': 'never', 'objects': 'never'}],
+		'eqeqeq': ['error', 'always' ],
+		'consistent-return': 'error',
+		'no-undef-init': 'error',
+		'no-undefined': 'error',
+		'no-void': 'error',
+		'no-var': 'error',
+		'object-shorthand': 'error',
+		'prefer-const': 'error',
+		'prefer-destructuring': ['error', {'array': true, 'object': true}],
+		'no-duplicate-imports': 'error',
+		'no-unused-private-class-members': 'error'
 	}
 };
